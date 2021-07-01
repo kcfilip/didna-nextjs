7479f780cd6ea02e3580241b0f87c039b662bab7
Filip Kostic <filip.kostic@theinfatuation.com>
2021-07-01 15:15:18 +0200

    Add titles

--- pages/ad-slots/index.js
+++ pages/ad-slots/index.js
@@ -4,13 +4,15 @@ const Component = () => {
   return (
     <div>
       <h1>Infatuation ad-slots</h1>
-      <p>
-        storage.googleapis.com/didna_hb/the_infatuation_inc/theinfatuation/didna_config.js
-      </p>
+      <p>site-leaderboard</p>
       <div className={styles.adSlot} id="site-leaderboard"></div>
+      <p>below-post</p>
       <div className={styles.adSlot} id="below-post"></div>
+      <p>right-rail</p>
       <div className={styles.adSlot} id="right-rail"></div>
+      <p>in-content-0</p>
       <div className={styles.adSlot} id="in-content-0"></div>
+      <p>in-content-1</p>
       <div className={styles.adSlot} id="in-content-1"></div>
     </div>
   );
--- pages/post/index.js
+++ pages/post/index.js
@@ -4,9 +4,11 @@ const Component = () => {
   return (
     <div>
       <h1>Post page</h1>
-      <p>below-post, in-content-0, in-content-1</p>
+      <p>below-post</p>
       <div className={styles.adSlot} id="below-post"></div>
+      <p>in-content-0</p>
       <div className={styles.adSlot} id="in-content-0"></div>
+      <p>in-content-1</p>
       <div className={styles.adSlot} id="in-content-1"></div>
     </div>
   );
